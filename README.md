e660028cc62636fabb0058e8a74f2275628a8b41
Dariusz Dziuk <me@dariuszdziuk.com>
2013-11-05 00:53:28 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,8 @@
+# WordNet.js
+
+Simple Node.js module for Princeton University's [WordNet](http://wordnet.princeton.edu/) dictionary.
+
+# License
+
+MIT License.
+Princeton University's [WordNet License](http://wordnet.princeton.edu/wordnet/license/)
---
+++ examples/lookup.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+/**
+ * WordNet lookup example
+ *
+ * @author Dariusz Dziuk <me@dariuszdziuk.com>
+ */
+
+var program = require('commander');
+var wordnet = require('../lib/wordnet.js');
+
+program
+  .version('0.0.1')
+  .usage('<word>')
+  .parse(process.argv);
+
+/* Word to look up */
+var word = program.args[0];
+if (!word) {
+  program.help();
+}
+
+wordnet.lookup(word, function(err, definition) {
+
+  if (err) {
+    console.log('An error occured: %s', err);
+    return;
+  }
+
+  console.log('[%s]\n\n%s', word, definition);
+
+});

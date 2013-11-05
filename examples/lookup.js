d8d380ed12675d2367facaf59011a009d2998975
Dariusz Dziuk <me@dariuszdziuk.com>
2013-11-05 22:51:03 +0100

    Better example.

--- examples/lookup.js
+++ examples/lookup.js
@@ -20,13 +20,28 @@ if (!word) {
   program.help();
 }
 
-wordnet.lookup(word, function(err, definition) {
+wordnet.lookup(word, function(err, definitions) {
 
   if (err) {
     console.log('An error occured: %s', err);
     return;
   }
 
-  console.log('[%s]\n\n', word, definition);
+  console.log('\n  %s\n', word);
+
+  /* Definitions */
+  definitions.forEach(function(definition) {
+
+    console.log('  type : %s', definition.meta.synsetType);
+    var words = '';
+    definition.meta.words.forEach(function(word) {
+      words += word.word + ' ';
+    });
+    console.log('  words: %s', words.trim());
+    console.log('  %s', definition.glossary);
+
+    console.log();
+
+  });
 
 });
